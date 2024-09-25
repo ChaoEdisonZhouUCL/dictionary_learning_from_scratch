f1e2c3fe1476e4607066faaa14356a196fa2635d
Chao Zhou <chao.zhou@cispa.de>
2024-09-25 15:47:29 +0200

    up

--- main_power.py
+++ main_power.py
@@ -418,9 +418,7 @@ if __name__ == "__main__":
         0.001
     ]  # [10**x for x in range(-4, 1)]   sparse regularization parameter
     # alpha_values.append(0.0)
-    n_orders = [
-        1,
-    ]
+    n_orders = [1, 2, 3, 4, 5]
     U_init_values = [0.1]
 
     #  Create a pool of worker processes
