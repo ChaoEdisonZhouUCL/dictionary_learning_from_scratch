fa96c5d23f17684d472fd3a7cf169da23e3cae1f
Chao Zhou <chao.zhou@cispa.de>
2024-09-25 13:51:25 +0200

    up

--- main_power.py
+++ main_power.py
@@ -216,7 +216,7 @@ class MiniBatchDictionaryLearning:
             n_samples = X.shape[0]
 
             # X_init = np.abs(np.random.normal(0, 1.0, (n_samples, self.n_components)))
-             X_init = np.ones((n_samples, self.n_components))
+            X_init = np.ones(n_samples, self.n_components)
             codes_U = np.power(
                 (X_init + np.sqrt(X_init**2 + self.alpha**2)) / 2,
                 1 / 2 * self.n,
@@ -273,7 +273,7 @@ class MiniBatchDictionaryLearning:
         a_prev = 0.01 * np.identity(self.n_components)
         b_prev = 0
         # X_init = np.abs(np.random.normal(0, 1.0, (n_samples, self.n_components)))
-        X_init = np.ones((n_samples, self.n_components))
+        X_init = np.ones(n_samples, self.n_components)
         codes_U_X = np.power(
             (X_init + np.sqrt(X_init**2 + self.alpha**2)) / 2,
             1 / 2 * self.n,
